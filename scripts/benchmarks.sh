7662a311652052a8bc288bc47abbb2f2953ac651
User Ryan <krhancoc@uwaterloo.ca>
2023-11-19 11:09:47 -0700

    Redis

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -47,4 +47,49 @@ kill -9 `pgrep lighttpd`
 
 }
 
-lighttpd_benchmark
+redis_benchmark()
+{
+
+ROOT=$(realpath "$(dirname "$0")/..")
+. $ROOT/scripts/util.sh
+
+PLOXD=/usr/home/ryan/ploxd
+
+mkdir -p $ROOT/out
+
+OUTPUT=$ROOT/out/redis.csv
+
+touch $OUTPUT
+
+kldload $PLOXD/kplox/kmod/plox.ko
+$PLOXD/build/src/ploxd/ploxd &
+
+run_redis_with_plox
+
+sleep 5
+
+for ITER in {1..5}
+do
+  echo "redis-benchmark $ITER"
+  run_redis_benchmark >> $OUTPUT
+done
+
+kill -SIGINT `pgrep ploxd`
+sleep 1
+kill -SIGINT `pgrep ploxd`
+sleep 1
+
+run_redis &
+
+sleep 5
+
+for ITER in {1..5}
+do
+  echo "redis-benchmark $ITER"
+  run_redis_benchmark >> $OUTPUT
+done
+
+kill -9 `pgrep redis`
+}
+
+redis_benchmark
